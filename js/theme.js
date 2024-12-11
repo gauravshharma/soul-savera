23c196fc58293128d1ad92dd281ba65712d329d7
gaurav <gaurav@gauravs-MacBook-Air.local>
2024-12-11 17:53:59 +0530

    ui fxes

--- js/theme.js
+++ js/theme.js
@@ -927,6 +927,16 @@ $(".option").click(function(){
   
 });
 
+// Load Header
+fetch('header.html')
+.then(response => response.text())
+.then(data => document.getElementById('header').innerHTML = data);
+
+// Load Footer
+fetch('footer.html')
+.then(response => response.text())
+.then(data => document.getElementById('footer').innerHTML = data);
+
 
     
 })(jQuery);
